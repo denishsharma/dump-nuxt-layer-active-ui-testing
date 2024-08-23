f524414572f27f966272e81bec4f276d8f2de20d
Denish Sharma <denishcommon@gmail.com>
2024-08-23 16:31:11 +0530

    chore: add core module with Button component

---
+++ modules/core/index.ts
@@ -0,0 +1,22 @@
+import { addComponentsDir, createResolver, defineNuxtModule, useLogger } from 'nuxt/kit'
+
+export default defineNuxtModule({
+  meta: {
+    name: "core-layer-module",
+    version: "0.0.1"
+  },
+  setup: async () => {
+    const logger = useLogger('core-layer-module')
+    const { resolve } = createResolver(import.meta.url)
+
+    logger.info('core-layer-module setup')
+
+    const runtimeDir = resolve('./runtime')
+
+    await addComponentsDir({
+      path: resolve(runtimeDir, 'components'),
+      extensions: ['vue'],
+      prefix: "ActiveLayer"
+    })
+  }
+})
---
+++ modules/core/runtime/components/Button.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    This is a button from modules/core
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
